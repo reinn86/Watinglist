abdf1a662c8f5aa05d1860c7043a1c106dcdc742
haruki takahashi <tanukichang@outlook.jp>
2023-10-04 12:45:07 +0900

    入力画面の仮UI作成

---
+++ src/main/java/application/controller/InputSceneController.java
@@ -0,0 +1,35 @@
+package application.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/input")
+public class InputSceneController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String viewName = "/WEB-INF/views/input/input.html";
+       
+//    public InputSceneController() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; UTF-8");
+		RequestDispatcher dispatch = request.getRequestDispatcher(viewName);
+		dispatch.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
