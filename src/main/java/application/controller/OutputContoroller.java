b5e36171812f811a591723b806f800e223bf8b02
haruki takahashi <tanukichang@outlook.jp>
2023-10-10 14:21:33 +0900

    コントローラの追加

--- src/main/java/application/controller/OutputContoroller.java
+++ src/main/java/application/controller/OutputContoroller.java
@@ -10,11 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/input")
-public class InputSceneController extends HttpServlet {
+/**
+ * Servlet implementation class OutputContoroller
+ */
+@WebServlet("/output")
+public class OutputContoroller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private String viewName = "/WEB-INF/views/input/input.html";
-
+	private String viewName = "/WEB-INF/views/input/output.html";
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		response.setCharacterEncoding("UTF-8");
